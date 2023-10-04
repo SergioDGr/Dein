7f35af1a84ebca085642f621b1e155782f0ec162
SergioDGr <sergiodanielgroppa@gmail.com>
2023-10-04 12:10:41 +0200

    ADD base ejercicio_a
    Creo la base para el ejercicio A

---
+++ ud1/src/application/ejercicio_a/EjercicioA.java
@@ -0,0 +1,23 @@
+package application.ejercicio_a;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * 
+ *  
+ * 
+ */
+public class EjercicioA extends Application{
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage stage) throws Exception {
+		
+		
+	}
+	
+}
--- ud1/src/module-info.java
+++ ud1/src/module-info.java
@@ -5,4 +5,13 @@
  * 
  */
 module ud1 {
+	requires javafx.controls;
+	requires java.desktop;
+	requires javafx.web;
+	requires javafx.fxml;
+	requires javafx.swing;
+	requires javafx.media;
+	requires javafx.graphics;
+	opens application to javafx.graphics, javafx.fxml;
+	opens controllers to javafx.graphics, javafx.fxml;
 }
