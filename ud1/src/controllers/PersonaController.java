88754f8ecf5c9bafa8a2462e87344567a4f9d5d3
SergioDGr <sergiodanielgroppa@gmail.com>
2023-10-10 20:06:41 +0200

    doc PersonaController
    En la clase controller se le añade la documentación

--- ud1/src/controllers/PersonaController.java
+++ ud1/src/controllers/PersonaController.java
@@ -48,20 +48,29 @@ public class PersonaController implements Initializable{
     
     private ObservableList<Persona> lstPesonas = FXCollections.observableArrayList();
     
+    /**
+     * Al darle click al boton intenta añadir una persona, muestra una alerta 
+     * si se a podido añadir o no la persona
+     * @param event
+     */
     @FXML
     void clic_addPersona(ActionEvent event) {
     	Alert alert = null;
-    	String mensajeErrores = validarCampos();
+    	String mensajeErrores = validarTextFields();
     	boolean aniadido = false;
+    	//En caso que no haya errores
     	if (mensajeErrores.isEmpty()) {
     		Persona p = new Persona(tfNombre.getText(), tfApellidos.getText(), Integer.parseInt(tfEdad.getText()));
+    		//Se intenta añadir la persona
     		aniadido = aniadirPersona(p);
     		if (aniadido) {
+    			//En caso de que se podido añadirlo
 		    	alert = new Alert(Alert.AlertType.INFORMATION);
 		    	alert.setContentText("Persona añadida correctamente");
 		    	alert.setTitle("Info");
     		}
     	}
+    	//Cuando hay algun error
     	if (! mensajeErrores.isEmpty() || !aniadido) {
     		if ( mensajeErrores.isEmpty() && !aniadido)
     			mensajeErrores = "La persona ya esta en el tabla\n";
@@ -69,13 +78,20 @@ public class PersonaController implements Initializable{
     		alert.setContentText(mensajeErrores);
     		alert.setTitle("Error");
     	}
-    	
+    	//Se visualiza la alerta
         alert.setHeaderText("");
         alert.showAndWait();
     }
     
-    private String validarCampos() {
+    /**
+     * Metodo que valida los textfield que tiene la informacion 
+     * cada apartado de la persona
+     * @return devuelve un mensaje con los errores que hay en caso que no
+     * haya devolvera vacio en mensaje
+     */
+    private String validarTextFields() {
     	String mensaje = "";
+    	//Validaciones
     	if(tfNombre.getText().isEmpty())
 			mensaje += "El campo nombre es obligatorio\n";
     	if(tfApellidos.getText().isEmpty())
@@ -90,18 +106,17 @@ public class PersonaController implements Initializable{
 			}
 
     	return mensaje;
-    	
     }
     
+    /**
+     * Metodo que añada a la lista de personas una persona
+     * @param p la persona a añadir
+     * @return devuelve si se a podido o no añadir la persona
+     */
     private boolean aniadirPersona(Persona p) {
-    	
-    	
-    	System.out.println( lstPesonas.size());
     	if(lstPesonas.contains(p))
     		return false;
-    	
     	lstPesonas.add(p);
-    	
     	return true;
     }
     
