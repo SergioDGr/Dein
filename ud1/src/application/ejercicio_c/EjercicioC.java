f4f6a0480f9771bec62da52017af5edadb0f0b1a
SergioDGr <sergiodanielgroppa@gmail.com>
2023-10-14 22:21:08 +0200

    Doc Ejercicio C
    Documentar la clase ejecicio 3

--- ud1/src/application/ejercicio_c/EjercicioC.java
+++ ud1/src/application/ejercicio_c/EjercicioC.java
@@ -7,8 +7,10 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 /**
- * 
- * 
+ * Ventana que muestra una tabla con 3 columnas Nombre, Apellidos y Edad y
+ *  3 TextField para rellenar los datos de la persona los mismos que la tabla;
+ *  y 3 botones, uno para añadir a la persona, otro para modificar y otro para
+ *  eliminar a la persona.
  */
 public class EjercicioC extends Application{
 
