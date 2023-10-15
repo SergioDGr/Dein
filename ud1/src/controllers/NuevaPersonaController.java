0f2842d1b1d1bf52cf1bde1484e34c42803ea738
SergioDGr <sergiodanielgroppa@gmail.com>
2023-10-15 05:13:38 +0200

    Add Interfaz del modal
    Modal que aparecerá cuando se le de al botón añadir de la interfaz principal

---
+++ ud1/src/controllers/NuevaPersonaController.java
@@ -0,0 +1,35 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class NuevaPersonaController {
+	
+	@FXML
+    private Button btnCancelar;
+
+    @FXML
+    private Button btnGuardar;
+
+    @FXML
+    private TextField tfApellidos;
+
+    @FXML
+    private TextField tfEdad;
+
+    @FXML
+    private TextField tfNombre;
+
+    @FXML
+    void click_cancelar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_guardar(ActionEvent event) {
+
+    }
+	
+}
