3c688cbaaef7153ef4015951202aded8164507c3
SergioDGr <sergiodanielgroppa@gmail.com>
2023-10-20 10:13:56 +0200

    Feat Imagen
    Añadir la imagen a la interfaz

--- ud1/src/controllers/PersonaController7.java
+++ ud1/src/controllers/PersonaController7.java
@@ -27,7 +27,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
-
+import javafx.scene.image.ImageView;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.Window;
