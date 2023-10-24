a5c8c74109b38b6a01796ebe235f9c39469fcade
SergioDGr <sergiodanielgroppa@gmail.com>
2023-10-24 19:10:49 +0200

    ADD Base Ejercicio K
    Crear base Ejercicio K

---
+++ ud1/src/application/ejercicio_k/EjercicioK.java
@@ -0,0 +1,31 @@
+package application.ejercicio_k;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+/**
+ *	Ventana que visualiza un coche, un boton para encender o apagar las luces del coche,
+ *  y varios botones para cambiar el color del coche
+ */
+public class EjercicioK extends Application{
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage stage) throws Exception {
+		GridPane root = (GridPane) FXMLLoader.load(getClass().getResource("/fxml/EjercicioJ.fxml"));
+		Scene scene = new Scene(root, 575, 550);
+		//scene.getStylesheets().add(getClass().getResource("/css/estilo_ejercicioJ.css").toExternalForm());
+		stage.setTitle("RELOJ");
+		stage.setScene(scene);
+		stage.getIcons().add(new Image(getClass().getResourceAsStream("/img/Icono.png")));
+		stage.show();
+	}
+	
+}
