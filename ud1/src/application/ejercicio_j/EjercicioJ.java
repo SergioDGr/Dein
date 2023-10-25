fc1a2da46ee647dd2a8ea28b852950914aec22a1
SergioDGr <sergiodanielgroppa@gmail.com>
2023-10-25 10:17:25 +0200

    Chore Imagen Icono
    Cambiada al icono correcto

--- ud1/src/application/ejercicio_j/EjercicioJ.java
+++ ud1/src/application/ejercicio_j/EjercicioJ.java
@@ -24,7 +24,7 @@ public class EjercicioJ extends Application{
 		scene.getStylesheets().add(getClass().getResource("/css/estilo_ejercicioJ.css").toExternalForm());
 		stage.setTitle("MINI COOPER");
 		stage.setScene(scene);
-		stage.getIcons().add(new Image(getClass().getResource("/img/cooperLogo.png").toString()));
+		stage.getIcons().add(new Image(getClass().getResource("/img/Cooper.png").toString()));
 		stage.show();
 	}
 	
