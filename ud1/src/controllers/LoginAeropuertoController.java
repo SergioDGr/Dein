c940bad3ef33ae097d2154f8c3627113d0ce0ea6
SergioDGr <sergiodanielgroppa@gmail.com>
2023-10-26 13:23:27 +0200

    Feat interfaz login
    Creamos la interfaz del login

---
+++ ud1/src/controllers/LoginAeropuertoController.java
@@ -0,0 +1,27 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginAeropuertoController {
+	
+	@FXML
+    private Button btnLogin;
+
+    @FXML
+    private PasswordField pfPass;
+
+    @FXML
+    private TextField tfUsuario;
+
+    @FXML
+    void click_login(ActionEvent event) {
+
+    }
+    
+}
