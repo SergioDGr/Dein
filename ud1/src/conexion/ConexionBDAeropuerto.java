680d3393c79c2f2551bee8da13bb245b924ce4da
SergioDGr <sergiodanielgroppa@gmail.com>
2023-10-28 12:21:29 +0200

    Fix Conexion
    Arreglada la configuración de la base de datos Aeropuertos

--- ud1/src/conexion/ConexionBDAeropuerto.java
+++ ud1/src/conexion/ConexionBDAeropuerto.java
@@ -1,13 +1,10 @@
 package conexion;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
+
+import java.util.ResourceBundle;
 import java.util.TimeZone;
 
 public class ConexionBDAeropuerto {
@@ -18,18 +15,14 @@ public class ConexionBDAeropuerto {
 	 * En el contructor se hara la conexion a la base de datos de aeropuertos
 	 * y se guardar en la propiedad conn
 	 * @throws SQLException si ocurre algun error se lanzara exception ralacionada a la base datos
-	 * @throws IOException si ocurre algun error en la lectura de archivo de configuracion
-	 * @throws FileNotFoundException si que no encuetre el archivo de configuracion
 	 */
-	public ConexionBDAeropuerto() throws SQLException, FileNotFoundException, IOException {
-		Properties propiedades = new Properties();
-		propiedades.load(new FileInputStream(getClass().getResource("/configuration.properties")
-				.toString()));
+	public ConexionBDAeropuerto() throws SQLException {
+		ResourceBundle bundle = ResourceBundle.getBundle("configuration");
 		
-		String host = propiedades.getProperty("connBD.host");
-		String baseDatos =  propiedades.getProperty("connBD.database");
-		String usuario = propiedades.getProperty("connBD.user");
-		String password = propiedades.getProperty("connBD.pass");
+		String host = bundle.getString("connBD.host");
+		String baseDatos =  bundle.getString("connBD.database");
+		String usuario = bundle.getString("connBD.user");
+		String password = bundle.getString("connBD.pass");
 		String cadenaConexion = "jdbc:mysql://" + host + "/" + baseDatos + "?serverTimezone=" + TimeZone.getDefault().getID();
 		conn = DriverManager.getConnection(cadenaConexion, usuario, password);
 		conn.setAutoCommit(true);
