5b582a60dd5c9cbd9ebf46753a998f46f19763ca
SergioDGr <sergiodanielgroppa@gmail.com>
2023-11-08 10:19:50 +0100

    add module-info productoLabel
    Añadido al archivo module-info el producto-label

--- ud1/src/module-info.java
+++ ud1/src/module-info.java
@@ -14,6 +14,7 @@ module ud1 {
 	requires javafx.graphics;
 	requires java.sql;
 	requires de.jensd.fx.glyphs.fontawesome;
+	requires ProductoLabel;
 	opens application.ejercicio_a to javafx.graphics, javafx.fxml;
 	opens application.ejercicio_b to javafx.graphics, javafx.fxml;
 	opens application.ejercicio_c to javafx.graphics, javafx.fxml;
