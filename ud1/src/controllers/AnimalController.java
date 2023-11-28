61df3c56a85709f2c543634da121cb305aa75411
SergioDGr <sergiodanielgroppa@gmail.com>
2023-11-28 08:30:26 +0100

    Feat interfaz
    Creada la interfaz del Ejercicio S

---
+++ ud1/src/controllers/AnimalController.java
@@ -0,0 +1,54 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+
+public class AnimalController {
+	
+	@FXML
+    private TableView<?> tableAnimales;
+
+    @FXML
+    private TableColumn<?, ?> tcEdad;
+
+    @FXML
+    private TableColumn<?, ?> tcEspecie;
+
+    @FXML
+    private TableColumn<?, ?> tcFecha;
+
+    @FXML
+    private TableColumn<?, ?> tcID;
+
+    @FXML
+    private TableColumn<?, ?> tcNombre;
+
+    @FXML
+    private TableColumn<?, ?> tcPeso;
+
+    @FXML
+    private TableColumn<?, ?> tcRaza;
+
+    @FXML
+    private TableColumn<?, ?> tcSexo;
+
+    @FXML
+    private TextField tfNombre;
+
+    @FXML
+    void click_addConsulta(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_verConsultas(ActionEvent event) {
+
+    }
+	
+}
