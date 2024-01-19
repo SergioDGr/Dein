eaf170a6844b11c30e53b20f5562e5cd7ea4b360
SergioDGr <sergiodanielgroppa@gmail.com>
2024-01-19 10:50:11 +0100

    Add Base Ejercicio 2
    Creado la base para el ejercicio 2

---
+++ ud3/src/application/Ejercicio2.java
@@ -0,0 +1,30 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+
+public class Ejercicio2 extends Application {
+	
+	@Override
+	public void start(Stage stage) {
+		try {
+			GridPane root = (GridPane) FXMLLoader.load(getClass().getResource("/fxml/informes.fxml"));
+			Scene scene = new Scene(root,600,550);
+	        //scene.getStylesheets().add(getClass() .getResource("/css/application.css").toExternalForm());
+			stage.setTitle("INFORMES");
+			stage.setScene(scene);
+			stage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+}
--- ud3/src/module-info.java
+++ ud3/src/module-info.java
@@ -1,5 +1,10 @@
 module ud3 {
 	requires javafx.controls;
+	requires javafx.web;
+	requires javafx.fxml;
+	requires javafx.swing;
+	requires javafx.media;
+	requires javafx.graphics;
 	requires jasperreports;
 	requires java.sql;
 	requires java.desktop;
